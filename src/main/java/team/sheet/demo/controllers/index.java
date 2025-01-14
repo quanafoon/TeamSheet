85f320e0430b654a856955a73341c365734c18f3
quanafoon <quanafoon@gmail.com>
2025-01-14 14:50:38 -0400

    started team sheets

--- src/main/java/team/sheet/demo/controllers/index.java
+++ src/main/java/team/sheet/demo/controllers/index.java
@@ -1,8 +1,6 @@
 package team.sheet.demo.controllers;
 
-import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 import team.sheet.demo.models.User;
 import team.sheet.demo.repositories.UserRepository;
+
+
 
 
 @Controller
@@ -24,15 +24,34 @@ public class Index {
         model.addAttribute(new User());
         return "index";
     }
+
     @PostMapping("/login")
     public String login(@ModelAttribute User user, Model model){
         Optional<User> optionalUser = userRepository.findByUsername(user.getUsername());
         if(optionalUser.isPresent()){
             User u = optionalUser.get();
             if (User.checkPassword(user.getPassword(), u.getPassword())) {
-                return "builder";
+                return "teams";
             }
         }
         return "redirect:/";
     }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute User user, Model model) {
+        Optional<User> optionalUser = userRepository.findByUsername(user.getUsername());
+        if(optionalUser.isPresent()){
+            return "redirect:/";
+        }
+        User newUser = new User(user.getUsername(), user.getPassword(), user.getFirstname(), user.getLastname());
+        userRepository.save(newUser);
+        return "teams";
+    }
+
+    @GetMapping("/builder")
+    public String teams() {
+        return "builder";
+    }
+    
+    
 }
--- src/main/java/team/sheet/demo/repositories/UserRepository.java
+++ src/main/java/team/sheet/demo/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package team.sheet.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.sheet.demo.models.User;
-import java.util.List;
 import java.util.Optional;
 
 
